26db6326d756e284c94e42c82f8789eede112bbf
jabilisunal <sunal.jabili@code.edu.az>
2024-05-15 13:24:47 +0400

    mongo db connection

--- index.js
+++ index.js
@@ -1,85 +1,62 @@
-const express = require("express");
-const port = 8080;
+import express, { json } from "express";
+import mongoose, { Schema } from "mongoose";
+import 'dotenv/config'
+
 const app = express();
-app.use(express.json());
+app.use(json());
 
-let categories = [
-  {
-    id: 1,
-    description: "Sweet and savory sauces relishes spreads and seasonings",
-    name: "Condiments",
-  },
-  {
-    id: 2,
-    description: "Soft drinks coffees teas beers and ales",
-    name: "Beverages",
-  },
-  {
-    id: 3,
-    description: "Desserts candies and sweet breads",
-    name: "Confections",
-  },
-  {
-    id: 4,
-    description: "Cheeses",
-    name: "Dairy Products",
-  },
-  {
-    id: 5,
-    description: "Breads crackers pasta and cereal",
-    name: "Grains/Cereals",
-  },
-  {
-    id: 6,
-    description: "Prepared meats",
-    name: "Meat/Poultry",
-  },
-  {
-    id: 7,
-    description: "Dried fruit and bean curd",
-    name: "Produce",
-  },
-  {
-    id: 8,
-    description: "Seaweed and fish",
-    name: "Seafood",
-  },
-];
-let counter = 1000;
+const productSchema = new Schema({
+  name: String,
+  price: Number,
+  category: String,
+  discountPrice: Number,
+});
+const productModel = mongoose.model("myProducts", productSchema);
 
-app.get("/categories", (req, res) => {
-  res.send(categories);
+app.get("/", async (req, res) => {
+  const products = await productModel.find({});
+  res.send(products);
 });
 
-app.get("/categories/:id", (req, res) => {
+app.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = categories.find((item) => item.id == id);
-  res.send(result);
+  const products = await productModel.findById(id);
+  res.send(products);
 });
 
-app.post("/categories", (req, res) => {
-  const { description, name, price } = req.body;
-  categories.push({ id: counter, name, description, price });
-  counter++;
-  res.send("Data ugurla yaradildi..");
+
+app.post("/", async (req, res) => {
+  const { name, price, category, discountPrice } = req.body;
+  const newProduct = new productModel({ name, price, category, discountPrice });
+  await newProduct.save();
+  res.status(200).send("Product yarandi..");
 });
 
-app.delete("/categories/:id", (req, res) => {
-  const { id } = req.params;
-  categories = categories.filter((item) => item.id !== +id);
-  res.send("Data silindi");
+app.delete("/:id", async(req,res)=>{
+  const {id}= req.params;
+  const product = await productModel.findByIdAndDelete(id);
+  res.send(product);
+})
+
+app.put("/:id", async(req,res)=>{
+  const {id}= req.params;
+  const { name, price, category, discountPrice } = req.body;
+  const product = await productModel.findByIdAndUpdate(id,{ name, price, category, discountPrice });
+  res.send(product);
+})
+
+app.put("/:id", (req, res) => {
+  res.send("Get element PUT");
+});
+console.log(process.env.PORT);
+
+mongoose
+  .connect(process.env.DB_SECRET_KEY)
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log("Not Connect!"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`this website running this ${process.env.PORT}`);
 });
 
-app.put("/categories/:id", (req, res) => {
-    const { id } = req.params;
-    const { description, name } = req.body;
-    counter++;
-    const index = categories.findIndex((item)=>item.id==id);
-    categories[index]= {id,description,name}
-    res.send("Update olund");
-  });
-
-
-app.listen(port, () => {
-  console.log(`Bu website bu portda calishir ${port} `);
-});
+// mongodb+srv://Sunal:123456backend@mycluster.4byfppp.mongodb.net/
